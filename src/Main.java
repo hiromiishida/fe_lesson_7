66fa4274b4b2a966337d11ab42e8423499d1f5a3
digitalplan <digitalplan21@gmail.com>
2022-10-28 11:20:57 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 11;
+
+        while(n <= 10){
+            System.out.println(n);
+            n = n + 1;
+        }
+    }
+}
